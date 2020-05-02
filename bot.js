6abcb2de5101a49de6305c2c5cb69c88f5691bd9
FinalCrisis <64656243+FinalCrisis@users.noreply.github.com>
2020-05-02 17:01:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
 const {Client, RichEmbed} = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
 const PREFIX = '$';
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === "Hola Kirin"){
         message.reply('Bienvenid@, disfruta de tu aventura en Final Crisis');
     }
 
 })
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('I am ready!');
     client.user.setActivity('la Crisis Final', { type: 'WATCHING'}).catch(console.error);
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
